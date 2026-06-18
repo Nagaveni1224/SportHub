a348698b5095aadb18098ed33ed680b7f0484ab0
nagavenig1224 <nagavebig1224@gmail.com>
2026-06-18 22:28:03 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">
+        SportHub
+      </h1>
+    </div>
+  );
+}
+
+export default App;
